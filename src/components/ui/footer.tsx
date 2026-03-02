71f31a16c62d16d5a7c3c5bba98694413fdd7200
Tharanga <tharanga@quintsn.com>
2026-03-02 15:57:48 +0530

    About folder rename

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
         <footer className="bg-[#1e2a47] text-white border-t border-[#2d3e6a]">
             <div className="max-w-7xl mx-auto px-6 md:px-12 py-8">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                    {/* Brand */}
+
                     <div>
                         <Link href={createPageUrl('Home')} className="inline-block mb-4">
                             <img
